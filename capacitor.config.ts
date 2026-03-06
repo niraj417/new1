6637243bb8ba625f84c973a0ee87802d7ed10508
niraj417 <129906912+niraj417@users.noreply.github.com>
2026-03-06 22:54:59 +0530

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.flowpay.app',
+  appName: 'FlowPay',
+  webDir: 'dist'
+};
+
+export default config;
